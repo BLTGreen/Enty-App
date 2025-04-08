573c71a91f4fea6dcc8b827a3d23cf92a2f513f3
BLTGreen <cleananovencleanme@gmail.com>
2025-04-08 11:57:20 +0100

    Setting up Enty with Slint

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+slint::slint! {
+    export { MainWindow } from "ui/app-window.slint";
+}
+
+fn main() -> Result<(), slint::PlatformError> {
+    let main_window = MainWindow::new()?;
+
+    main_window.run()
+}
